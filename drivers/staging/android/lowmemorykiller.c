a4f8eafbe58da37d76dae133cda330f44e7e1082
Christopher <cristoforo.cataldo@gmail.com>
2013-04-27 13:14:38 +0200

    lowmemorykiller: Updated LMK from 3.4 kernel

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -116,6 +116,7 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 		if (!p)
 			continue;
 
+
 		if (test_tsk_thread_flag(p, TIF_MEMDIE) &&
 		    time_before_eq(jiffies, lowmem_deathpending_timeout)) {
 			task_unlock(p);
