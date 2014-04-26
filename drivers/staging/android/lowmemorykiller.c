a5e964988b8997905462d7a0f02d223ade364278
Alberto García <garcia98ftw@gmail.com>
2014-04-27 00:31:28 +0200

    lowmemorykiller: Fixed errors and warnings

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -155,6 +155,7 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 	int tasksize;
 	int i;
 	int min_score_adj = OOM_SCORE_ADJ_MAX + 1;
+	int minfree = 0;
 	enum lowmem_process_type proc_type = KILLABLE_PROCESS;
 	int selected_tasksize[MANAGED_PROCESS_TYPES] = {0};
 	int selected_oom_score_adj[MANAGED_PROCESS_TYPES];
@@ -313,7 +314,8 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 					 minfree * (long)(PAGE_SIZE / 1024),
 					 min_score_adj,
 					 other_free * (long)(PAGE_SIZE / 1024));
-			lowmem_deathpending_timeout = jiffies + HZ;
+			lowmem_deathpending_timeout = ktime_add_ns(ktime_get(),
+								   NSEC_PER_SEC/2);
 			send_sig(SIGKILL, selected[proc_type], 0);
 			set_tsk_thread_flag(selected[proc_type], TIF_MEMDIE);
 			rem -= selected_tasksize[proc_type];
@@ -322,8 +324,6 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 	}
 
 	rcu_read_unlock();
-	/* give the system time to free up the memory */
-	msleep_interruptible(20);
 
 	lowmem_print(4, "lowmem_shrink %lu, %x, return %d\n",
 		     sc->nr_to_scan, sc->gfp_mask, rem);
