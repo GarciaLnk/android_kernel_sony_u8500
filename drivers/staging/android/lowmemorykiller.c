c9b69e9d2982aaa849fb2f3a6375c5a770526f2b
Alberto García <garcia98ftw@gmail.com>
2014-05-20 01:08:09 +0200

    Revert "lowmemorykiller: Updated to 3.4"
    This reverts commit 483e6c4543ed7fa2b23d6b3730b6981fc46b502f.

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -18,6 +18,7 @@
  * and processes may not get killed until the normal oom killer is triggered.
  *
  * Copyright (C) 2007-2008 Google, Inc.
+ * Copyright (C) 2012 Sony Mobile Communications AB.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -35,9 +36,11 @@
 #include <linux/mm.h>
 #include <linux/oom.h>
 #include <linux/sched.h>
-#include <linux/swap.h> 
+#include <linux/swap.h>
 #include <linux/rcupdate.h>
 #include <linux/notifier.h>
+#include <linux/ktime.h>
+#include <linux/spinlock.h>
 #ifdef CONFIG_ANDROID_LOW_MEMORY_KILLER_DO_NOT_KILL_PROCESS
 #include <linux/string.h>
 #endif
@@ -58,7 +61,9 @@ static int lowmem_minfree[6] = {
 };
 static int lowmem_minfree_size = 4;
 
-static unsigned long lowmem_deathpending_timeout;
+static ktime_t lowmem_deathpending_timeout;
+
+#define LMK_BUSY (-1)
 
 #ifdef CONFIG_ANDROID_LOW_MEMORY_KILLER_DO_NOT_KILL_PROCESS
 #define MAX_NOT_KILLABLE_PROCESSES	25	/* Max number of not killable processes */
@@ -133,14 +138,21 @@ enum lowmem_process_type {
 #define lowmem_print(level, x...)			\
 	do {						\
 		if (lowmem_debug_level >= (level))	\
-			pr_info(x);			\
+			printk(x);			\
 	} while (0)
 
 static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 {
+	static DEFINE_SPINLOCK(lowmem_lock);
 	struct task_struct *tsk;
 	struct task_struct *selected[MANAGED_PROCESS_TYPES] = {NULL};
 	int rem = 0;
+	static int same_count;
+	static int busy_count;
+	static int busy_count_dropped;
+	static int oldpid;
+	static int lastpid;
+	static ktime_t next_busy_print;
 	int tasksize;
 	int i;
 	int min_score_adj = OOM_SCORE_ADJ_MAX + 1;
@@ -151,23 +163,31 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 	int array_size = ARRAY_SIZE(lowmem_adj);
 	int other_free = global_page_state(NR_FREE_PAGES) - totalreserve_pages;
 	int other_file = global_page_state(NR_FILE_PAGES) -
-						global_page_state(NR_SHMEM);
+						global_page_state(NR_FILE_MAPPED);
 
 	if (lowmem_adj_size < array_size)
 		array_size = lowmem_adj_size;
 	if (lowmem_minfree_size < array_size)
 		array_size = lowmem_minfree_size;
 	for (i = 0; i < array_size; i++) {
-		minfree = lowmem_minfree[i];
-		if (other_free < minfree && other_file < minfree) {
+		/*
+		 * Convert lowmem_minfree[i] to signed to avoid that other_free
+		 * and/or other_file are converted to unsigned.
+		 *
+		 */
+		if (other_free < (int) lowmem_minfree[i] &&
+		    other_file < (int) lowmem_minfree[i]) {
 			min_score_adj = lowmem_adj[i];
 			break;
 		}
 	}
 	if (sc->nr_to_scan > 0)
-		lowmem_print(3, "lowmem_shrink %lu, %x, ofree %d %d, ma %d\n",
+		lowmem_print(3, "lowmem_shrink %lu, %x, ofree %d %d (%lu %lu), ma %d\n",
 				sc->nr_to_scan, sc->gfp_mask, other_free,
-				other_file, min_score_adj);
+				other_file,
+				global_page_state(NR_FILE_PAGES),
+				global_page_state(NR_FILE_MAPPED),
+				min_score_adj);
 	rem = global_page_state(NR_ACTIVE_ANON) +
 		global_page_state(NR_ACTIVE_FILE) +
 		global_page_state(NR_INACTIVE_ANON) +
@@ -175,6 +195,7 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 	if (sc->nr_to_scan <= 0 || min_score_adj == OOM_SCORE_ADJ_MAX + 1) {
 		lowmem_print(5, "lowmem_shrink %lu, %x, return %d\n",
 			     sc->nr_to_scan, sc->gfp_mask, rem);
+
 		return rem;
 	}
 
@@ -182,6 +203,20 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 	for (proc_type = KILLABLE_PROCESS; proc_type < MANAGED_PROCESS_TYPES; proc_type++)
 		selected_oom_score_adj[proc_type] = min_score_adj;
 
+	if (spin_trylock(&lowmem_lock) == 0) {
+		if (ktime_us_delta(ktime_get(), next_busy_print) > 0) {
+			lowmem_print(2, "Lowmemkiller busy %d %d %d\n",
+				busy_count, busy_count_dropped,
+				oom_killer_disabled);
+			next_busy_print = ktime_add(ktime_get(),
+						    ktime_set(5, 0));
+			busy_count_dropped = 0;
+		}
+		busy_count++;
+		busy_count_dropped++;
+		return LMK_BUSY;
+	}
+	/* turn of scheduling to protect task list */
 	rcu_read_lock();
 	for_each_process(tsk) {
 		struct task_struct *p;
@@ -195,10 +230,30 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 			continue;
 
 		if (test_tsk_thread_flag(p, TIF_MEMDIE) &&
-		    time_before_eq(jiffies, lowmem_deathpending_timeout)) {
+			ktime_us_delta(ktime_get(),
+				lowmem_deathpending_timeout) < 0) {
 			task_unlock(p);
+			same_count++;
+			if (p->pid != oldpid || same_count > 1000) {
+				lowmem_print(1,
+					"terminate %d (%s) old:%d last:%d %ld %d\n",
+					p->pid, p->comm, oldpid, lastpid,
+					(long)ktime_us_delta(ktime_get(),
+						lowmem_deathpending_timeout),
+					same_count);
+				lowmem_print(2,
+					"state:%ld flag:0x%x la:%lld busy:%d %d\n",
+					p->state, p->flags,
+					p->sched_info.last_arrival,
+					busy_count, oom_killer_disabled);
+				oldpid = p->pid;
+				same_count = 0;
+			}
 			rcu_read_unlock();
-			return 0;
+			spin_unlock(&lowmem_lock);
+			/* wait one jiffie */
+			schedule_timeout(1);
+			return LMK_BUSY;
 		}
 		oom_score_adj = p->signal->oom_score_adj;
 		if (oom_score_adj < min_score_adj) {
@@ -236,7 +291,7 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 		selected[proc_type] = p;
 		selected_tasksize[proc_type] = tasksize;
 		selected_oom_score_adj[proc_type] = oom_score_adj;
-		lowmem_print(2, "select %d (%s), adj %d, size %d, to kill\n",
+		lowmem_print(4, "select %d (%s), adj %d, size %d, to kill\n",
 			     p->pid, p->comm, oom_score_adj, tasksize);
 	}
 
@@ -248,10 +303,20 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 	 *   if so kill it to prevent system slowdowns, hangs, etc. */
 	for (proc_type = KILLABLE_PROCESS; proc_type < MANAGED_PROCESS_TYPES; proc_type++) {
 		if (selected[proc_type]) {
-			lowmem_print(1, "send sigkill to %d (%s), adj %d, size %d\n",
-			     selected[proc_type]->pid, selected[proc_type]->comm,
-			     selected_oom_score_adj[proc_type], selected_tasksize[proc_type]);
-			lowmem_deathpending_timeout = jiffies + HZ;
+			lowmem_print(1, "Killing '%s' (%d), adj %d,\n" \
+					"   to free %ldkB on behalf of '%s' (%d) because\n" \
+					"   cache %ldkB is below limit %ldkB for oom_score_adj %d\n" \
+					"   Free memory is %ldkB above reserved\n",
+					 selected[proc_type]->comm, selected[proc_type]->pid,
+					 selected_oom_score_adj[proc_type],
+					 selected_tasksize[proc_type] * (long)(PAGE_SIZE / 1024),
+					 current->comm, current->pid,
+					 other_file * (long)(PAGE_SIZE / 1024),
+					 minfree * (long)(PAGE_SIZE / 1024),
+					 min_score_adj,
+					 other_free * (long)(PAGE_SIZE / 1024));
+			lowmem_deathpending_timeout = ktime_add_ns(ktime_get(),
+								   NSEC_PER_SEC/2);
 			send_sig(SIGKILL, selected[proc_type], 0);
 			set_tsk_thread_flag(selected[proc_type], TIF_MEMDIE);
 			rem -= selected_tasksize[proc_type];
@@ -259,9 +324,12 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 		}
 	}
 
+	rcu_read_unlock();
+
 	lowmem_print(4, "lowmem_shrink %lu, %x, return %d\n",
 		     sc->nr_to_scan, sc->gfp_mask, rem);
 	rcu_read_unlock();
+	spin_unlock(&lowmem_lock);
 	return rem;
 }
 
@@ -299,3 +367,4 @@ module_init(lowmem_init);
 module_exit(lowmem_exit);
 
 MODULE_LICENSE("GPL");
+
